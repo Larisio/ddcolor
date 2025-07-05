13d7c04807eb3a193a87b847df00c729184e1a82
Larisio <multilirs@gmail.com>
2025-07-05 14:22:25 +0200

    Update train.sh

--- scripts/train.sh
+++ scripts/train.sh
@@ -1,3 +1,3 @@
-CUDA_VISIBLE_DEVICES=0 \
+CUDA_VISIBLE_DEVICES=1 \
 #python -m torch.distributed.launch --nproc_per_node=4 --master_port=3721 basicsr/train.py -opt options/train/train_ddcolor.yml --auto_resume --launcher pytorch
 torchrun --nproc_per_node=1 --master_port=3721 /home/lkmm73/ddcolor/basicsr/train.py -opt  /home/lkmm73/ddcolor/options/train/train_ddcolor.yml --auto_resume --launcher pytorch
