880c5101da4a0fcd3db163c03417159b3df80574
Larisio <multilirs@gmail.com>
2025-07-06 16:44:55 +0200

    add short run opt

---
+++ scripts/train_small.sh
@@ -0,0 +1,2 @@
+
+CUDA_VISIBLE_DEVICES=0 torchrun --nproc_per_node=1 --master_port=3721 /home/lkmm73/ddcolor/basicsr/train.py -opt  /home/lkmm73/ddcolor/options/train/train_ddcolor_short.yml --auto_resume --launcher pytorch
